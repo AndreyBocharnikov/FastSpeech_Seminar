516a44c594338bcf0a7df43e0fb70fce6ca3044c
Andrey Bocharnikov <a.bocharnikov@corp.vk.com>
2021-12-01 13:24:28 +0300

    try training

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
 import yaml
+from pytorch_lightning import Trainer
 
 from fastspeech_model.dataset import FastSpeech2DataModule
+from fastspeech_model.fastspeech2 import FastSpeech2Model
 
 if __name__ == "__main__":
     with open('fastspeech2_train.yaml') as f:
         cfg = yaml.load(f)
 
-    FastSpeech2DataModule(**cfg['dataset'])
+    datamodule = FastSpeech2DataModule(**cfg['dataset'])
+    model = FastSpeech2Model(**cfg['model'])
+    trainer = Trainer()
+    trainer.fit(model, datamodule)
